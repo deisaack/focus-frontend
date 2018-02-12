5c46e38a8e27f7a06130191167a8ba51c0996110
De Isaac <deisaack@gmail.com>
2018-02-12 19:59:38 +0300

    with bootstrap and font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {SidebarModule} from 'ng-sidebar';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AuthComponent
   ],
   imports: [
-    BrowserModule, SidebarModule.forRoot(),
+    BrowserModule, SidebarModule.forRoot(), NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
